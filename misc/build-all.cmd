6a5a8f76d0ecc7f10423648028cd0be3baf926fd
Konstantin Vlasov <support@flint-inc.ru>
2021-02-18 00:57:23 +0300

    Updated to 6.1.18

--- misc/build-all.cmd
+++ misc/build-all.cmd
@@ -40,7 +40,7 @@ echo echo.>> build-tmp.cmd
 echo echo ### %VERSION%: BUILDING x64 VERSION ###>> build-tmp.cmd
 echo echo.>> build-tmp.cmd
 echo set BUILD_TARGET_ARCH=amd64>> build-tmp.cmd
-echo cscript configure.vbs --with-DDK=C:\WinDDK\7600.16385.1 --with-MinGW-w64=C:\Programs\mingw64 --with-MinGW32=C:\Programs\mingw32 --with-libSDL=C:\Programs\SDL\x64 --with-openssl=C:\Programs\OpenSSL\x64 --with-openssl32=C:\Programs\OpenSSL\x32 --with-libcurl=C:\Programs\curl\x64 --with-libcurl32=C:\Programs\curl\x32 --with-Qt5=C:\Programs\Qt\5.6.3-x64 --with-libvpx=C:\Programs\libvpx --with-libopus=C:\Programs\libopus --with-python=C:/Programs/Python>> build-tmp.cmd
+echo cscript configure.vbs --with-DDK=C:\WinDDK\7600.16385.1 --with-MinGW-w64=C:\Programs\mingw64 --with-libSDL=C:\Programs\SDL\x64 --with-openssl=C:\Programs\OpenSSL\x64 --with-openssl32=C:\Programs\OpenSSL\x32 --with-libcurl=C:\Programs\curl\x64 --with-libcurl32=C:\Programs\curl\x32 --with-Qt5=C:\Programs\Qt\5.6.3-x64 --with-libvpx=C:\Programs\libvpx --with-libopus=C:\Programs\libopus --with-python=C:/Programs/Python>> build-tmp.cmd
 echo if ERRORLEVEL 1 exit /b ^1>> build-tmp.cmd
 echo call env.bat>> build-tmp.cmd
 echo kmk>> build-tmp.cmd
@@ -69,51 +69,7 @@ if NOT ".%WIN10_MS_SIGN%" == ".0" (
 	echo cd %WORKING_DIR%>> build-tmp.cmd
 )
 echo echo ### Building the 64-bit MSI>> build-tmp.cmd
-echo kmk %WORKING_DIR_NIX%out/win.x86/release/obj/Installer/VirtualBox-%VERSION%-MultiArch_amd64.msi>> build-tmp.cmd
-echo if ERRORLEVEL 1 exit /b ^1>> build-tmp.cmd
-
-cmd /c build-tmp.cmd
-if ERRORLEVEL 1 exit /b 1
-
-del /q build-tmp.cmd 2>nul
-
-echo @echo off>> build-tmp.cmd
-echo call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /Release /x86 /win7>> build-tmp.cmd
-echo color 07>> build-tmp.cmd
-echo echo.>> build-tmp.cmd
-echo echo ### %VERSION%: BUILDING x32 VERSION ###>> build-tmp.cmd
-echo echo.>> build-tmp.cmd
-echo set BUILD_TARGET_ARCH=x86>> build-tmp.cmd
-echo cscript configure.vbs --with-DDK=C:\WinDDK\7600.16385.1 --with-MinGW-w64=C:\Programs\mingw64 --with-MinGW32=C:\Programs\mingw32 --with-libSDL=C:\Programs\SDL\x32 --with-openssl=C:\Programs\OpenSSL\x32 --with-libcurl=C:\Programs\curl\x32 --with-Qt5=C:\Programs\Qt\5.6.3-x32 --with-libvpx=C:\Programs\libvpx --with-libopus=C:\Programs\libopus --with-python=C:/Programs/Python>> build-tmp.cmd
-echo if ERRORLEVEL 1 exit /b ^1>> build-tmp.cmd
-echo call env.bat>> build-tmp.cmd
-echo kmk>> build-tmp.cmd
-echo if ERRORLEVEL 1 exit /b ^1>> build-tmp.cmd
-if NOT ".%WIN10_MS_SIGN%" == ".0" (
-	echo echo ### Signing 32-bit drivers for Windows 10>> build-tmp.cmd
-	echo set SRC_FILE=VBoxDrivers-%VERSION_CAB%-x86.cab>> build-tmp.cmd
-	echo cd %WORKING_DIR%out\win.x86\release\repack>> build-tmp.cmd
-	echo call PackDriversForSubmission.cmd -x -a x86 -o %%SRC_FILE%%>> build-tmp.cmd
-	echo if ERRORLEVEL 1 exit /b ^1>> build-tmp.cmd
-	echo set KBUILD_DEVTOOLS=%WORKING_DIR%tools>> build-tmp.cmd
-	echo set KBUILD_BIN_PATH=%WORKING_DIR%kBuild\bin\win.x86>> build-tmp.cmd
-	echo set _MY_SIGNTOOL=%VBOX_PATH_SIGN_TOOLS%\signtool.exe>> build-tmp.cmd
-	echo set DST_FILE=VBoxDrivers-%VERSION_CAB%-x86-mssigned.zip>> build-tmp.cmd
-	echo call sign-dual.cmd %%SRC_FILE%%>> build-tmp.cmd
-	echo if ERRORLEVEL 1 exit /b ^1>> build-tmp.cmd
-	echo python %WORKING_DIR%sign_ms.py %%SRC_FILE%% %%DST_FILE%%>> build-tmp.cmd
-	echo if ERRORLEVEL 1 exit /b ^1>> build-tmp.cmd
-	echo call UnpackBlessedDrivers.cmd -n -i %%DST_FILE%%>> build-tmp.cmd
-	echo if ERRORLEVEL 1 exit /b ^1>> build-tmp.cmd
-	echo set KBUILD_DEVTOOLS=>> build-tmp.cmd
-	echo set KBUILD_BIN_PATH=>> build-tmp.cmd
-	echo set _MY_SIGNTOOL=>> build-tmp.cmd
-	echo set SRC_FILE=>> build-tmp.cmd
-	echo set DST_FILE=>> build-tmp.cmd
-	echo cd %WORKING_DIR%>> build-tmp.cmd
-)
-echo echo ### Building the full installer>> build-tmp.cmd
-echo kmk %WORKING_DIR_NIX%out/win.x86/release/bin/VirtualBox-%VERSION%-MultiArch.exe>> build-tmp.cmd
+echo kmk %WORKING_DIR_NIX%out/win.amd64/release/bin/VirtualBox-%VERSION%-MultiArch.exe>> build-tmp.cmd
 echo if ERRORLEVEL 1 exit /b ^1>> build-tmp.cmd
 
 cmd /c build-tmp.cmd
